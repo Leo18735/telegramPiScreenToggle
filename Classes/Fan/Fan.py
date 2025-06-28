e5444c4f52158d53589bfb6c02b87fd35b59ffc5
Lang, Simon <simon01209@gmail.com>
2025-06-28 16:21:53 +0200

    Remove hasattribute

--- Classes/Fan/Fan.py
+++ Classes/Fan/Fan.py
@@ -5,15 +5,16 @@ from Classes.Fan.GPIO import GPIO
 class Fan:
     def __init__(self, pin: int, *_, **__):
         self._pin: int = pin
-        self._state: State = self.set(State.OFF)
+        self._state: State = State.ERROR
+        self._state = self.set(State.OFF)
 
     def set(self, state: State) -> State:
-        if (hasattr(self, "_state") and state == self._state) or state == state.NO_CHANGE:
+        if state == self._state or state == state.NO_CHANGE:
             return State.NO_CHANGE
         if state == State.BLOCK:
             GPIO.set_state(self._pin, State.OFF)
         else:
-            if hasattr(self, "_state") and self._state == State.BLOCK and state != State.OFF:
+            if self._state == State.BLOCK and state != State.OFF:
                 return self._state
             GPIO.set_state(self._pin, state)
         self._state = state
