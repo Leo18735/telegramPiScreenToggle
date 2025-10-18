42defb487db23db67659b002d29e758e02fb1166
Lang, Simon <simon01209@gmail.com>
2025-10-18 17:57:04 +0200

    Fix temperaturecontroller

--- Classes/Controller/TemperatureController.py
+++ Classes/Controller/TemperatureController.py
@@ -9,7 +9,7 @@ class TemperatureController(BaseExecutionController):
         stdout, stderr, code = self._execute(self._base_command)
         if code != 0:
             raise Exception(f"Could not get temperature. {stderr}")
-        return re.findall(r"temp=(\d+\.\d+)'C", stdout)[0]
+        return float(re.findall(r"temp=(\d+\.\d+)'C", stdout)[0])
 
     def set_state(self):
         raise NotImplementedError
