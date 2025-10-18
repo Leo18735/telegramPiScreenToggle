78aaf6ec505952aa0abf7a9b89a5bcf2be087656
Lang, Simon <simon01209@gmail.com>
2025-10-18 18:38:56 +0200

    Make blocking when blocked possible

--- Classes/Controller/BaseGpioController.py
+++ Classes/Controller/BaseGpioController.py
@@ -3,6 +3,7 @@ import abc
 import RPi.GPIO
 
 from Classes.Controller.BaseController import BaseController
+import threading
 
 
 class BaseGpioController(BaseController, abc.ABC):
@@ -12,14 +13,20 @@ class BaseGpioController(BaseController, abc.ABC):
         self._blocked: bool = False
         self._set_state("off")
 
+        self._lock: threading.Lock = threading.Lock()
+
     def _set_state(self, state: str):
-        if state != "unblock" and self._blocked:
+        self._lock.acquire()
+        if state not in ["unblock", "block"] and self._blocked:
+            self._lock.release()
             raise Exception("Fan is blocked")
         elif state == "block":
             self._blocked = True
+            self._lock.release()
             return
         elif state == "unblock":
             self._blocked = False
+            self._lock.release()
             return
         new_state = state == "on"
         self._state = new_state
@@ -27,3 +34,4 @@ class BaseGpioController(BaseController, abc.ABC):
         RPi.GPIO.setmode(RPi.GPIO.BCM)
         RPi.GPIO.setup(self._pin, RPi.GPIO.OUT)
         RPi.GPIO.output(self._pin, RPi.GPIO.HIGH if self._state else RPi.GPIO.LOW)
+        self._lock.release()
