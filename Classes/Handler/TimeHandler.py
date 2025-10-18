168d6254c377aeb873338a82fc3322ebf0ea63db
Lang, Simon <simon01209@gmail.com>
2025-10-18 18:18:41 +0200

    Fix

--- Classes/Handler/TimeHandler.py
+++ Classes/Handler/TimeHandler.py
@@ -14,7 +14,7 @@ class Task:
     @classmethod
     def from_dict(cls, data: dict):
         hour, minute = data.get("time").split(":")
-        data["time"] = datetime(year=1970, month=1, day=1, hour=int(hour), minute=int(minute))
+        data["time"] = datetime(year=1970, month=1, day=1, hour=int(hour), minute=int(minute), second=0)
         return cls(**data)
 
 
